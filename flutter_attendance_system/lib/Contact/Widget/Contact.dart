37fd38d95ab4e1941204a69f20f376feb6f440aa
TienDat <drdat1415@gmail.com>
2020-04-02 20:31:03 +0700

    update contact fb and youtube

--- flutter_attendance_system/lib/Contact/Widget/Contact.dart
+++ flutter_attendance_system/lib/Contact/Widget/Contact.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class Contact extends StatelessWidget {
   @override
@@ -12,7 +14,8 @@ class Contact extends StatelessWidget {
           shrinkWrap: true,
           children: <Widget>[
             Padding(
-              padding: EdgeInsets.only(left: 10, right: 10, bottom: 20, top: 10),
+              padding:
+                  EdgeInsets.only(left: 10, right: 10, bottom: 20, top: 10),
               child: Container(
                   height: 120, child: Image.asset("images/default.png")),
             ),
@@ -96,62 +99,123 @@ class Contact extends StatelessWidget {
               padding: EdgeInsets.only(left: 10, right: 10, bottom: 20),
               child: Center(
                   child: Wrap(
+                children: <Widget>[
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 10, right: 10, bottom: 10),
+                    child:
+                        Center(child: Image.asset("images/location-arrow.png")),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 10, right: 10, bottom: 10),
+                    child: Center(
+                        child: Text("Cơ sở",
+                            style: GoogleFonts.lobster(fontSize: 20))),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 10, right: 10, bottom: 10),
+                    child: Center(
+                        child: Text(
+                            "681/24/9 Âu Cơ, P.Tân Thành, Q.Tân Phú, TP. Hồ Chí Minh",
+                            style: GoogleFonts.farro(fontSize: 16))),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 10, right: 10, bottom: 10),
+                    child: Center(child: Image.asset("images/office.png")),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 10, right: 10, bottom: 10),
+                    child: Center(
+                        child: Text("Văn phòng đại diện",
+                            style: GoogleFonts.lobster(fontSize: 20))),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 10, right: 10, bottom: 10),
+                    child: Center(
+                        child: Text(
+                            "41, Lê Bình, Phường 4, Quận Tân Bình, TP. Hồ Chí Minh",
+                            style: GoogleFonts.farro(fontSize: 16))),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 10, right: 10, bottom: 10),
+                    child: Center(child: Image.asset("images/phone-arrow.png")),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 10, right: 10, bottom: 10),
+                    child: Center(
+                        child: Text("Điện thoại",
+                            style: GoogleFonts.lobster(fontSize: 20))),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 10, right: 10, bottom: 10),
+                    child: Center(
+                        child: Text("079 999 7407",
+                            style: GoogleFonts.farro(fontSize: 16))),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 10, right: 10, bottom: 10),
+                    child: Center(child: Image.asset("images/mail-arrow.png")),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 10, right: 10, bottom: 10),
+                    child: Center(
+                        child: Text("E-mail",
+                            style: GoogleFonts.lobster(fontSize: 20))),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 10, right: 10, bottom: 10),
+                    child: Center(
+                        child: Text("contact@fpo.vn",
+                            style: GoogleFonts.farro(fontSize: 16))),
+                  ),
+                ],
+              )),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 10, right: 10),
+              child: Center(
+                  child: Text('2020 \u00a9 bản quyền thuộc về FPO Co.,Ltd')),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 10, right: 10),
+              child: Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.only(left: 10, right: 10, bottom: 10),
-                        child: Center(child: Image.asset("images/location-arrow.png")),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 10, right: 10, bottom: 10),
-                        child: Center(child: Text("Cơ sở", style: GoogleFonts.lobster(fontSize: 20))),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 10, right: 10, bottom: 10),
-                        child: Center(child: Text("681/24/9 Âu Cơ, P.Tân Thành, Q.Tân Phú, TP. Hồ Chí Minh",style: GoogleFonts.farro(fontSize: 16))),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 10, right: 10, bottom: 10),
-                        child: Center(child: Image.asset("images/office.png")),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 10, right: 10, bottom: 10),
-                        child: Center(child: Text("Văn phòng đại diện", style: GoogleFonts.lobster(fontSize: 20))),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 10, right: 10, bottom: 10),
-                        child: Center(child: Text("41, Lê Bình, Phường 4, Quận Tân Bình, TP. Hồ Chí Minh",style: GoogleFonts.farro(fontSize: 16))),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 10, right: 10, bottom: 10),
-                        child: Center(child: Image.asset("images/phone-arrow.png")),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 10, right: 10, bottom: 10),
-                        child: Center(child: Text("Điện thoại", style: GoogleFonts.lobster(fontSize: 20))),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 10, right: 10, bottom: 10),
-                        child: Center(child: Text("079 999 7407",style: GoogleFonts.farro(fontSize: 16))),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 10, right: 10, bottom: 10),
-                        child: Center(child: Image.asset("images/mail-arrow.png")),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 10, right: 10, bottom: 10),
-                        child: Center(child: Text("E-mail", style: GoogleFonts.lobster(fontSize: 20))),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 10, right: 10, bottom: 10),
-                        child: Center(child: Text("contact@fpo.vn",style: GoogleFonts.farro(fontSize: 16))),
-                      ),
+                      IconButton(
+                          icon: Icon(
+                            FontAwesomeIcons.facebook,
+                            color: Colors.blue,
+                            size: 30,
+                          ),
+                          onPressed: (){
+                            Navigator.of(context).push(MaterialPageRoute(builder: (context) => Webviewer("https://www.facebook.com/FPOCompany/"),
+                            ));
+                          }),
+                      IconButton(
+                          icon: Icon(
+                            FontAwesomeIcons.youtube,
+                            color: Colors.red,
+                            size: 30,
+                          ),
+                          onPressed: (){
+                            Navigator.of(context).push(MaterialPageRoute(builder: (context) => Webviewer("https://www.youtube.com/channel/UC2xymGloIl8sL6ljhwbymsg"),
+                            ));
+                          }),
                     ],
                   )),
             ),
-            Padding(
-              padding: const EdgeInsets.only(left: 10,right: 10),
-              child: Center(child: Text('2020 \u00a9 bản quyền thuộc về FPO Co.,Ltd')),
-            )
           ],
         ),
       ),
@@ -164,5 +228,22 @@ class MyBehavior extends ScrollBehavior {
   Widget buildViewportChrome(
       BuildContext context, Widget child, AxisDirection axisDirection) {
     return child;
+  }
+}
+
+class Webviewer extends StatelessWidget {
+  final String url;
+
+  Webviewer(this.url);
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: WebView(
+          initialUrl: url,
+          javascriptMode: JavascriptMode.unrestricted,
+        ),
+      ),
+    );
   }
 }
